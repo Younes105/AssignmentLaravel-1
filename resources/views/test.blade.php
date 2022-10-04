e4af8528db92f0d90883f70851078067f8b4cf53
Younes105 <younesuniversitydesktop2020@gmail.com>
2022-10-04 08:26:29 +0300

    ASS1_Laravel

---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.mylayout')
+@section('content')
+
+This is Test Page
+@endsection
---
+++ resources/views/testhome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<p>This is test Home Page</p>
+@endsection
